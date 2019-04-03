641ef5bb4d3341a8338e1fd98784a49b07b27b78
Thomas-Tuttle <48684599+Thomas-Tuttle@users.noreply.github.com>
2019-04-03 02:14:52 -0400

    project

--- assignments/closure.js
+++ assignments/closure.js
@@ -1,17 +1,33 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
 
+const animal = 'Turtle';
+  function returnAnimal () { 
+    return animal; }
+
+console.log(returnAnimal());
 
 /* STRETCH PROBLEMS, Do not attempt until you have completed all previous tasks for today's project files */
 
 
 // ==== Challenge 2: Create a counter function ====
 const counter = () => {
+  let count = 0;
+  return function () {
+    count = count + 1;
+    return count;
+  }
   // Return a function that when invoked increments and returns a counter variable.
 };
-// Example usage: const newCounter = counter();
-// newCounter(); // 1
-// newCounter(); // 2
+const newCounter = counter();
+ console.log(newCounter());  
+ console.log(newCounter());
+ console.log(newCounter());
+ console.log(newCounter());
+ console.log(newCounter());
+ console.log(newCounter());
+ console.log(newCounter());
+ console.log(newCounter());  
 
 // ==== Challenge 3: Create a counter function with an object that can increment and decrement ====
 const counterFactory = () => {
@@ -19,3 +35,18 @@ const counterFactory = () => {
   // `increment` should increment a counter variable in closure scope and return it.
   // `decrement` should decrement the counter variable and return it.
 };
+function newFunction() {
+  add();
+}
+
+
+
+
+
+
+
+
+
+
+console.log(`The ${returnAnimal()} won the race!`)
+
